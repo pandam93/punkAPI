68b5e21469e354a0bca2c481975e43f88c13b98a
Carlos Millan <cfmillanm@gmail.com>
2021-06-08 20:32:13 +0200

    Solucion usando async/await, pendiente de mejorar

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const beerCardTemplate = function (beer) {
-    return `<div class="beer">
+  return `<div class="beer">
                 <img class="img-beer" src="${beer.image_url}" alt="beer" />
                 <h1>${beer.name}</h1>
                 <p class="title">${beer.tagline}</p>
                 <p><a href="https://api.punkapi.com/v2/beers/${beer.id}">Show more</a></p>
               </div>`;
-  };
+};
 
-fetch("https://api.punkapi.com/v2/beers")
-.then(function (response) {
-  // fetch() returns a promise containing the response (a Response object).
-  // This is just an HTTP response, not the actual JSON. 
-  // To extract the JSON body content from the response, 
-  // we use the json() method and pass it into the next .then()
+let beerContainer = document.getElementById("container-beers");
 
-  return response.json();
-})
-.then(function (beers){
-    let beerContainer = document.getElementById('container-beers');
-    beers.forEach(element => {
-        beerContainer.innerHTML += beerCardTemplate(element);
-    });
-})
+// ASYNC/AWAIT
+const loadBeers = async () => {
+  const beers = await fetch("https://api.punkapi.com/v2/beers");
+  return beers.json();
+};
+
+loadBeers().then((value) =>
+  value.forEach((element) => {
+    beerContainer.innerHTML += beerCardTemplate(element);
+  })
+);
